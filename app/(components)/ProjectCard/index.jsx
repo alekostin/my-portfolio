508b9a9691be3ab18872eceac23e767ff355faad
alekostin <ostinkostin22@gmail.com>
2023-11-25 12:03:25 +0100

    added seo

--- app/(components)/ProjectCard/index.jsx
+++ app/(components)/ProjectCard/index.jsx
@@ -5,7 +5,7 @@ export default function ProjectCard({ title, link, img }) {
     <a href={link} target="_blank">
       <div className="w-full h-80 border-[1px] border-blue-600 overflow-hidden relative rounded-lg group">
         <Image
-          className="object-cover translate-y-0 group-hover:-translate-y-[70%] transition-transform duration-[7s]"
+          className="object-cover translate-y-0 group-hover:-translate-y-[70%] transition-transform duration-[3s]"
           width={350}
           height={350}
           src={img}
--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+"use client";
+import { useEffect } from "react";
+
 import Banner from "./(components)/Banner";
 import Aboutme from "./(components)/Aboutme";
 import Experience from "./(components)/Expirience";
@@ -7,13 +10,19 @@ import Contacts from "./(components)/Contacts";
 /* import Footer from "./(components)/Footer"; */
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
   return (
     <main className="w-full bg-gray-950 text-gray-300 px-6 md:px-12">
       <div className="max-w-screen-xl mx-auto lg:pt-[0.8rem]">
         <Banner />
+        <Aboutme />
         <Projects />
         <Experience />
-        <Aboutme />
         <Skills />
         <Contacts />
         {/* <Footer /> */}
@@ -21,7 +30,3 @@ export default function Home() {
     </main>
   );
 }
-
-export const metadata = {
-  title: "Oleksand Kostin | Portfolio",
-};
